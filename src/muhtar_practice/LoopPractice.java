ae2a48c57d1bbe256c34e7e751628f208007e140
IBRAHIM ARSLAN <ibrahimarslan35@gmail.com>
2020-02-29 15:39:00 -0500

    Added loop practice file

---
+++ src/muhtar_practice/LoopPractice.java
@@ -0,0 +1,9 @@
+package muhtar_practice;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Counter :: "+i);
+        }
+    }
+}
